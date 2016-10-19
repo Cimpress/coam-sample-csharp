92e16c002e988ac0fa55a1bea216721b78a5ee65
Christopher Baldauf <cbaldauf@vistaprint.com>
2016-10-19 15:19:01 -0400

    Initial commit

---
+++ iam_sample_csharp/Auth0TokenFetcher.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+
+namespace iam_sample_csharp
+{
+	public class Auth0TokenFetcher
+	{
+		private string clientId;
+		private string clientSecret;
+
+		public Auth0TokenFetcher(string clientId, string clientSecret)
+		{
+			this.clientId = clientId;
+			this.clientSecret = clientSecret;
+		}
+
+		public string GetToken()
+		{
+			var client = new RestClient("https://cimpress-dev.auth0.com");
+
+			var request = new RestRequest("oauth/token", Method.POST);
+			request.AddParameter("client_id", this.clientId); 
+			request.AddParameter("client_secret", this.clientSecret);
+			request.AddParameter("audience", "https://development.api.cimpress.io/");
+			request.AddParameter("grant_type", "client_credentials");
+
+			IRestResponse response = client.Execute(request);
+			dynamic json = Newtonsoft.Json.Linq.JObject.Parse(response.Content);
+			return json["access_token"];
+		}
+	}
+}
---
+++ iam_sample_csharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RestSharp;
+
+namespace iam_sample_csharp
+{
+	public class Program
+	{
+	    public static void Main()
+		{
+			// Get a token from Auth0 that will be used to call IAM
+			var fetcher = new Auth0TokenFetcher("{YOUR CLIENT ID HERE}", "{YOUR CLIENT SECRET}");
+			var iamToken = fetcher.GetToken();
+
+			// Make a request to IAM for permissions
+			var sub = "adfs|jshu@cimpress.com"; // Most commonly the sub field of the JWT that your app accepts
+			var resourceType = "merchants";
+			var merchant = "vistaprint";
+
+			var client = new RestClient("https://development.api.cimpress.io/iam/v0");
+			// var client = new RestClient("https://api.cimpress.io/auth/iam/v0"); // Production. Note the extra /auth path segment
+
+			// USER PERMISSIONS FOR ALL MERCHANTS
+			var request = new RestRequest("user-permissions/{sub}/{resourceType}", Method.GET);
+			request.AddHeader("Authorization", "Bearer " + iamToken);
+			request.AddUrlSegment("sub", sub);
+			request.AddUrlSegment("resourceType", resourceType);
+
+			IRestResponse response = client.Execute(request);
+			Console.WriteLine(response.Content);
+
+			// USER PERMISSIONS FOR A SINGLE MERCHANT
+			request = new RestRequest("user-permissions/{sub}/{resourceType}/{resourceIdentifier}", Method.GET);
+			request.AddHeader("Authorization", "Bearer " + iamToken);
+			request.AddUrlSegment("sub", sub);
+			request.AddUrlSegment("resourceType", resourceType);
+			request.AddUrlSegment("resourceIdentifier", merchant);
+
+
+			response = client.Execute(request);
+			Console.WriteLine(response.Content);
+		}
+	}
+}
